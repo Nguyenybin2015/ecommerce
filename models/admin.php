a8ee473dfb904c610d27ad24458e135e1f67cfa1
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-10-31 12:21:11 +0700

    ahihi

--- models/admin.php
+++ models/admin.php
@@ -1,16 +1,24 @@
 <?php
 require_once '../services/jwtHandler.php';
-class Admin{
-  public function __construct() {
-    
+require_once '../config/database.php';
+
+
+class Admin
+{
+  private $db;
+  private $connection;
+  public function __construct()
+  {
+    $this->db = new DatabaseService();
+    $this->connection = $this->db->getConnection();
   }
-  function addProduct(){
-    
+  function addProduct()
+  {
   }
-  function updateProduct(){
-    
+  function updateProduct()
+  {
   }
-  function delProduct(){
-    
+  function delProduct()
+  {
   }
 }
