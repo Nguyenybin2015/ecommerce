45e48b65077fa615ae62615810ede39987653a25
Bin Nguyễn <nguyenybin2015@gmail.com>
2023-11-27 22:17:32 +0700

    design pattern data mapper

--- models/product.php
+++ models/product.php
@@ -1,49 +1,71 @@
 <?php
+
 require_once '../config/database.php';
+
+class ProductMapper
+{
+    private $connection;
+
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getProductById($id_shoe)
+    {
+        $id_shoe = mysqli_real_escape_string($this->connection, $id_shoe);
+
+        $sql = "SELECT *
+                FROM `shoes` INNER JOIN `information` ON `information`.`id_information` = `shoes`.`id_information`
+                WHERE `shoes`.`id_shoe` = '$id_shoe'";
+
+        $query = mysqli_query($this->connection, $sql);
+        return mysqli_fetch_assoc($query);
+    }
+
+    public function getAllProducts()
+    {
+        $arrShoe = array();
+
+        $sql = "SELECT *
+                FROM `shoes` INNER JOIN `information` ON `information`.`id_information` = `shoes`.`id_information`";
+
+        $query = mysqli_query($this->connection, $sql);
+
+        if (mysqli_num_rows($query) > 0) {
+            while ($row = mysqli_fetch_assoc($query)) {
+                array_push($arrShoe, $row);
+            }
+        }
+
+        return $arrShoe;
+    }
+}
+
 class Product
 {
-  private $db;
-  private $connection;
-  public function __construct()
-  {
-    $this->db = new DatabaseService();
-    $this->connection = $this->db->getConnection();
-  }
+    private $db;
+    private $productMapper;
 
-  function getProduct($getParams)
-  {
-    $id_shoe = mysqli_real_escape_string($this->connection, $getParams['id_shoe']);
-
-    $sql = "SELECT *
-            FROM `shoes` inner join `information` on `information`.`id_information` = `shoes`.`id_information`
-            WHERE `shoes`.`id_shoe`= '$id_shoe'";
-
-    $query = mysqli_query($this->connection, $sql);
-    return mysqli_fetch_assoc($query);
-  }
-  function getAllProduct()
-  {
-    $arrShoe = array();
-
-    $sql = "SELECT *
-            FROM `shoes` inner join `information` on `information`.`id_information` = `shoes`.`id_information`";
-
-    $query = mysqli_query($this->connection, $sql);
-    if (mysqli_num_rows($query) > 0) {
-      while ($row = mysqli_fetch_assoc($query)) {
-        array_push($arrShoe, $row);
-      }
+    public function __construct()
+    {
+        $this->db = new DatabaseService();
+        $this->productMapper = new ProductMapper($this->db->getConnection());
     }
-    return $arrShoe;
-  }
 
-  function getOrderProduct($id_shoe)
-  {
-    $sql = "SELECT *
-            FROM `shoes` inner join `information` on `information`.`id_information` = `shoes`.`id_information`
-            WHERE `shoes`.`id_shoe`= '$id_shoe'";
+    public function getProduct($getParams)
+    {
+        $id_shoe = $getParams['id_shoe'];
+        return $this->productMapper->getProductById($id_shoe);
+    }
 
-    $query = mysqli_query($this->connection, $sql);
-    return mysqli_fetch_assoc($query);
-  }
+    public function getAllProduct()
+    {
+        return $this->productMapper->getAllProducts();
+    }
+
+    public function getOrderProduct($id_shoe)
+    {
+        return $this->productMapper->getProductById($id_shoe);
+    }
 }
--- models/shoppingCart.php
+++ models/shoppingCart.php
@@ -1,59 +1,97 @@
 <?php
+
 require_once '../config/database.php';
 require_once '../models/product.php';
 
+class OrderRepository
+{
+    private $connection;
+
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function addOrder($id_user, $id_shoe, $quantity)
+    {
+        $id_order = uniqid();
+
+        $sql = "INSERT INTO `order`(`id_order`, `id_user`, `id_shoe`, `quantity`) 
+                VALUES('$id_order', '$id_user', '$id_shoe', '$quantity')";
+
+        return mysqli_query($this->connection, $sql);
+    }
+
+    public function removeOrder($id_order)
+    {
+        $id_order = mysqli_real_escape_string($this->connection, $id_order);
+
+        $sql = "DELETE FROM `order` WHERE `id_order` = '$id_order'";
+        return mysqli_query($this->connection, $sql);
+    }
+
+    public function updateOrderQuantity($id_order, $quantity)
+    {
+        $id_order = mysqli_real_escape_string($this->connection, $id_order);
+        $quantity = mysqli_real_escape_string($this->connection, $quantity);
+
+        $sql = "UPDATE `order`
+                SET `quantity` = '$quantity'  
+                WHERE `id_order` = '$id_order'";
+        return mysqli_query($this->connection, $sql);
+    }
+
+    public function getAllOrdersByUser($id_user)
+    {
+        $id_user = mysqli_real_escape_string($this->connection, $id_user);
+
+        $sql = "SELECT `id_order`, `id_shoe`, `quantity` 
+                FROM `order`
+                WHERE `id_user` = '$id_user'";
+
+        $query = mysqli_query($this->connection, $sql);
+        return mysqli_fetch_all($query, MYSQLI_ASSOC);
+    }
+}
 
 class ShoppingCart
 {
-  private $db;
-  private $connection;
-  public function __construct()
-  {
-    $this->db = new DatabaseService();
-    $this->connection = $this->db->getConnection();
-  }
-  function addProduct($id_user, $id_shoe, $quantity){
-    $id_order = uniqid();
+    private $db;
+    private $orderRepository;
+    private $product;
 
-    $sql = "INSERT INTO `order`(`id_order`, `id_user`, `id_shoe`, `quantity`) 
-            VALUES('$id_order', '$id_user', '$id_shoe', '$quantity')";
-    
-    return mysqli_query($this->connection, $sql);
-  }
-  
-  function removeProduct($delParams){
-    $id_order = mysqli_real_escape_string($this->connection, $delParams['id_order']);
+    public function __construct()
+    {
+        $this->db = new DatabaseService();
+        $this->orderRepository = new OrderRepository($this->db->getConnection());
+        $this->product = new Product();
+    }
 
-    $sql = "DELETE FROM `order` WHERE `id_order` = '$id_order'";
-    return mysqli_query($this->connection, $sql);
-  }
+    public function addProduct($id_user, $id_shoe, $quantity)
+    {
+        return $this->orderRepository->addOrder($id_user, $id_shoe, $quantity);
+    }
 
-  function updateProductQuantity($quantityParams){
-    $id_order = mysqli_real_escape_string($this->connection, $quantityParams['id_order']);
-    $quantity = mysqli_real_escape_string($this->connection, $quantityParams['quantity']);
+    public function removeProduct($delParams)
+    {
+        return $this->orderRepository->removeOrder($delParams['id_order']);
+    }
 
-    $sql = "UPDATE `order`
-            SET `quantity` = '$quantity'  
-            WHERE `id_order` = '$id_order'";
-    return mysqli_query($this->connection, $sql);
-  }
-  function getAllProduct($getParams){
-    $id_user = mysqli_real_escape_string($this->connection, $getParams['id_user']);
+    public function updateProductQuantity($quantityParams)
+    {
+        return $this->orderRepository->updateOrderQuantity($quantityParams['id_order'], $quantityParams['quantity']);
+    }
 
-    $temp = new Product();
-    $arrShoe = array();
+    public function getAllProduct($getParams)
+    {
+        $id_user = $getParams['id_user'];
+        $arrOrders = $this->orderRepository->getAllOrdersByUser($id_user);
+        
+        $arrShoe = array();
+        foreach ($arrOrders as $order) {
+            $arrShoe[] = $this->product->getOrderProduct($order['id_shoe']);
+        }
 
-    $sql = "SELECT `id_shoe`, `quantity` 
-            FROM `order`
-            WHERE `id_user` = '$id_user'";
-            
-    $query = mysqli_query($this->connection, $sql);
-
-    if (mysqli_num_rows($query) > 0) {
-      while($row = mysqli_fetch_assoc($query)) {
-          array_push($arrShoe, $temp->getOrderProduct($row["id_shoe"]));
-      }
-    } 
-  return $arrShoe;
-  }
+        return $arrShoe;
+    }
 }
